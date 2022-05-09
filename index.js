7e666d0cd6ab6462a7ce3ed6170152682e0c69e2
JorgeHTcoding <jht.coding@gmail.com>
2022-05-09 13:03:13 +0200

    Subiendo todos los archivos del reto1 dia 6

---
+++ index.js
@@ -0,0 +1,26 @@
+let resultado;
+
+ let sum = (a, b = 0) =>{
+    return resultado = a + b;
+ }
+
+ let subs = (a, b = 0) =>{
+    return resultado = a - b;
+ }
+
+ let mult= (a, b = 0) =>{
+    return resultado = a * b;
+ }
+
+ let div = (a, b = 0) =>{
+    return resultado = a / b;
+ }
+ let square = (a = 0) =>{
+    return resultado = a*a;
+ }
+
+ module.exports= {sum, subs, mult, div, square}
+
+
+
+
---
+++ run.js
@@ -0,0 +1,7 @@
+let myLib = require("./index");
+
+console.log(myLib.div(3,4));
+console.log(myLib.sum(3,4));
+console.log(myLib.subs(3,4));
+console.log(myLib.mult(3,4));
+console.log(myLib.square(3));
